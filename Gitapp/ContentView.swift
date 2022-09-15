7e765a1d9a744bcc6ab14a75ada0f5b61ac756df
appviews <veena.sn366@gmail.com>
2022-09-15 14:52:09 -0400

    Initial Commit

---
+++ Gitapp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Gitapp
+//
+//  Created by VeenaMBP on 9/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gitapp/GitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitappApp.swift
+//  Gitapp
+//
+//  Created by VeenaMBP on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GitappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
